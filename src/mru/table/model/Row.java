d4b604dc33e607559ddb89e02668550c9561c656
Fenna Kort <fenna.kort@tutanota.com>
2026-03-03 19:03:53 -0700

    working on Row updates to rewrite compareTo and equals methods
    goal is to accommodate new use of array of strings for cell data; want
    to ensure that once ID values are compared, rows will be sorted
    alphabetically by first different cell value if IDs are equal

--- src/mru/table/model/Row.java
+++ src/mru/table/model/Row.java
@@ -3,6 +3,12 @@ package mru.table.model;
 public class Row implements Comparable<Row> {
 	// TODO update Row To Contain An Array of Strings
 	// TODOUpdate compare methods to reflect that it's now an array instead of a single string
+//	[x] constructors, one to create a row with a comma separated string and one to create a row with an array of strings;
+//	 [x] getters and setters;
+//	 [x] .toString() method;
+//	 [] The class should implement Comparable with the natural ordering being by the id.
+//	 [x] Since the value of the row is now an array you will need to provide methods to access and set those column values
+	
 	private int id; // id representing the row's order/location in the table.
 	private String[] values; // the cells to be associated with the row
 	
@@ -23,15 +29,24 @@ public class Row implements Comparable<Row> {
 	}
 	
 	/**
-	 * constructor To Create a new row object with a row id that has been incremented by one 
-	 * @param id the integer to use as the row id
+	 * constructor To Create a new row object using an array of strings
+	 * @param id the integer to use as the row id; recommend using an idCounter int inside the table class to increment the row numbers
 	 * @param values the cell Data of the row as An Array
 	 */
-	public Row(int id, String[]  values) {
+	public Row(int id, String[] values) {
 		this.id = id;
 		this.values =  values;
 	}
 	
+	/**
+	 * constructor To Create a new row object using a comma-separated string
+	 * @param id the integer to use as the row id; recommend using an idCounter int inside the table class to increment the row numbers
+	 * @param values the cell Data of the row as An Array
+	 */
+	public Row(int id, String values) {
+		this.id = id;
+		this.values = values.split(",");
+	}
 	
 //getters and setters
 	/**
@@ -41,36 +56,52 @@ public class Row implements Comparable<Row> {
 	public int getId() {return id;}
 	
 	/**
-	 * sets the id of the row. the id should correspond to the row's order/location in the table
+	 * sets the id of the row. the id should correspond to the row's order/location in the table; recommend using an idCounter int inside the table class to increment the row numbers
 	 * @param id the id to set
 	 */
 	public void setId(int id) {this.id = id;}
 	
 	
 	/**
-	 * retrieves the cell data associated with the row
-	 * @return the row's  cell data as  an array
+	 * retrieves an array containing all the cells associated with the row
+	 * @return the row's  cells as  an array
 	 */
 	public String[] getValues() {return values;}
 	
 	/**
-	 * sets the  cell data that should be associated with the row
+	 * sets the data to be stored in all cells of the row using an array
 	 * @param  values the row's cell data as  an array
 	 */
 	public void setValues(String[] values) {this.values = values;}
-
-
-
 	
+	/**
+	 * sets the data to be stored in all cells of the row using a comma-separated string
+	 * @param  values the row's cell data as comma-separated string
+	 */
+	public void setValues(String values) {this.values = values.split(",");}
+
+	/**
+	 * retrieves the data stored in the target cell of the row
+	 * @param i the target index of the  cell
+	 * @return  the string stored at the target index
+	 */
+	public String getCell(int i) {return this.values[i];}
+
+	/**
+	 * sets the data to be stored in a specific cell of the row
+	 * @param i the target index of the cell to store the new data in
+	 * @param data  the new string to store in a specified cell
+	 */
+	public void setCell(int i, String data) {this.values[i] = data;}
 	
 	
 	
 //UtilityMethods
 
 	/**
-	 *tests whether or not another row has the same id and text as the current row.  if true, the two objects represent the same row in the table
+	 *tests whether or not another row has the same id and cell Data as the current row.  if true, the two objects represent the same row in the table
 	 *@param o another Row object to test For equivalency
-	 *@return true if both objects contain the same id and text values; false if not
+	 *@return true if both objects contain the same id and Cell Data values; false if not
 	 * @throws NullPointerException 
 	 */
 	public boolean equals(Row o) throws NullPointerException {
@@ -78,11 +109,21 @@ public class Row implements Comparable<Row> {
 		if (o == null) { //o contains no object reference
 			//no change to var equals necessary
 			throw new NullPointerException("Row o cannot be used as a parameter for comparison, as o is null");
-		} else if (o.getText() == null) {
-			throw new NullPointerException("Row " + o.getId() +"'s text value is null");
+		} else if (o.getValues() == null) {
+			throw new NullPointerException("Row " + o.getId() +" contains no cell data");
 		}
-		else if ((o.getId() == this.id) && (o.getText().compareTo(this.text) == 0)) { // if id and text both contain the same values, row o equals Current Row
-			equals = true;
+		else if ((o.getId() == this.id) && (o.getValues().length == this.getValues().length)) { // if ids are the same and the  rows store the same number of columns,  compare cell by cell
+			boolean cellByCell = false;			
+			for (int i = 0; i < o.getValues().length; i++) {
+				if (this.getValues()[i].equals(o.getValues()[i])) {
+					cellByCell = true; //this can't work because it could return true if only last cells are identical
+				}
+			}
+			equals = cellByCell;
+			
+			if (compareAllCells(o) == 0) {
+				equals = true;
+			}
 		}
 		// if neither of the previous conditions are satisfied, the two objects are not equal; no change to var equals is necessary
 		return equals; 
@@ -95,10 +136,10 @@ public class Row implements Comparable<Row> {
 	public int compareTo(Row o) {
 		int compare = 0;
 		try {
-			if (!this.equals(o)) { //if o is not null, check that o does not represent the same row as current row
+			if (!this.equals(o)) { //if o does not represent the same row as current row
 					if (this.id < o.getId()) { //this.id comes before o.id
 						compare = -1;
-					} else if (this.id> o.getId()) {  //this.id comes after o.id
+					} else if (this.id > o.getId()) {  //this.id comes after o.id
 						compare = 1;
 					}
 				} else {
@@ -111,12 +152,44 @@ public class Row implements Comparable<Row> {
 		return compare;
 	}
 	
+	
+	//TODO maybe this belongs in a separate comparator class??
+	//if one cell is different, row is different, sort row alphabetically by different cell
+	public int compareAllCells(Row o) {
+		int compare = 0;
+		try {
+			do {
+				//compare each pair of cells
+				for (int i = 0; i < o.getValues().length; i++) {
+					compare = this.getValues()[i].compareTo(o.getValues()[i]);
+				}
+			} while(compare == 0); //continue until a difference is found, or for all cells
+			
+			//if a difference was found, convert comparison to either 1 or -1 because String.compareTo() returns a greater range of positive or negative ints
+			if (compare < 0) {
+				compare = -1; //this row precedes o according to alphabetical order in difference 
+			} else if (compare > 0) {
+				compare = 1; //this row follows o according to alphabetical order in difference
+			}
+			
+		} catch (NullPointerException e) {
+			System.out.println(e.getMessage());
+		}
+		return compare;
+	}
+	
 	 @Override
 	/**
-	 * formats the row's id and text with a tab between them for output, ie., "1	hello"
+	 * formats the row's id and cell data with a tab between them for output, ie., "1	hello	hi"
 	 */
 	public String toString() {
-		return this.id + "\t"+this.text;
+		 String output = Integer.toString(this.id);
+		 
+		 for (int i = 0; i <  this.values.length;i++) {
+			 output += "\t"+this.values[i];
+		 }
+		 
+		 return output;
 		
 	}
 
--- test/RowTest.java
+++ test/RowTest.java
@@ -8,20 +8,20 @@ import mru.table.model.Row;
 
 class RowTest {
 	private int idCounter = 1;
-	private Row r1 = new Row(idCounter++,"meow");
-	private Row r2 = new Row(idCounter++,"purr");
+	private Row r1 = new Row(idCounter++, new String[]{"meow","prow"});
+	private Row r2 = new Row(idCounter++, new String[]{"purr","mlow"});
 	private Row r3 = new Row(r1); //using the copy constructor for the row class in order to test the equals() method
 	
 	@Test
 	void testRowConstructor() {
 		assertEquals(1, r1.getId());
-		assertEquals("meow", r1.getText());
+		assertEquals("meow", r1.getValues()[0]);
 	}
 
 	@Test
 	void testIdIterator() {
-		r3 = new Row(idCounter++, "nya");
-		Row r4 = new Row(idCounter++, "prrowww");
+		r3 = new Row(idCounter++, new String[]{"purr","mlow"});
+		Row r4 = new Row(idCounter++, new String[]{"mrow","mlep"});
 		assertEquals(4, r4.getId());
 	}
 	
